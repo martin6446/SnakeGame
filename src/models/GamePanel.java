5c11f41fe4036837c14f241c65450a39b4654eca
Martin Caminero <martincaminero64@gmail.com>
2021-02-25 15:40:05 -0300

    getting the snake moving

--- src/models/GamePanel.java
+++ src/models/GamePanel.java
@@ -5,15 +5,14 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener {
 
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 25;
-	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
 	static final int DELAY = 75;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
@@ -25,68 +24,138 @@ public class GamePanel extends JPanel implements ActionListener{
 	boolean running = false;
 	Timer timer;
 	Random random;
-	
-	
-	GamePanel(){
+
+	GamePanel() {
 		random = new Random();
-		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setBackground(Color.black);
+		this.setFocusable(true);
 		this.addKeyListener(new MyKeyAdapter());
 		startGame();
 	}
-	
+
 	public void startGame() {
 		newAppel();
 		running = true;
-		timer = new Timer(DELAY,this);
+		timer = new Timer(DELAY, this);
 		timer.start();
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
 	}
-	
-	
+
 	public void draw(Graphics g) {
-		
-		for(int i = 0; i< SCREEN_HEIGHT/UNIT_SIZE; i++) {
-			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH,i*UNIT_SIZE);
+
+		// this is a grid
+		for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+		}
+
+		g.setColor(Color.red);
+		g.fillOval(appelX, appelY, UNIT_SIZE, UNIT_SIZE);
+
+		for (int i = 0; i < bodyParts; i++) {
+			if (i == 0) {
+				g.setColor(Color.green);
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			} else {
+				g.setColor(new Color(143, 187, 153));
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+
+			}
 		}
 	}
-	
+
 	public void newAppel() {
-		
+		appelX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+		appelY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 	}
-	
+
 	public void move() {
-		
+		for (int i = bodyParts; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
+
+		switch (direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
 	}
-	
+
 	public void checkAppel() {
-		
+
 	}
-	
+
 	public void checkCollisions() {
-		
+		// checks if head collides with body
+		for (int i = bodyParts; i > 0; i--) {
+			if ((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
+
+		// check if head touches left border
+		if (x[0] == 0) {
+			running = false;
+		}
+
+		// check if head touches right border
+		if (x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+
+		// check if head touches top border
+		if (y[0] < 0) {
+			running = false;
+		}
+
+		// check if head touches bottom border
+
+		if (y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+
+		if (!running) {
+			timer.stop();
+		}
+
 	}
-	
+
 	public void gameOver(Graphics g) {
-		
+
 	}
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+
+		if (running) {
+			move();
+			checkAppel();
+			checkCollisions();
+			System.out.println("asdf");
+		}
+		repaint();
+
 	}
-	
+
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+
 		}
 	}
 
